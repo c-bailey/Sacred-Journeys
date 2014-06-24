c11d4632419bc4d314ce9e934ce2b295f7d3d48a
Chelsea <chelsea_bailey@md-20013613.wgbh.org>
2014-06-24 14:41:58 -0400

    styling based on specs!

--- anchorAnimated.js
+++ anchorAnimated.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
 	var duration = 1000;
 	var $root = $('html, body');
 
-	$('a').click(function(event) {
+	$('a[href^="#"]').click(function(event) {
 		event.preventDefault();
 		$root.animate({scrollTop:$( $(this).attr('href')).position().top}, duration, 'linear');
 		return false;
