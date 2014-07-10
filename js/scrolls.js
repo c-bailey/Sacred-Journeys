9ccc29c6b6e238e3b4a42597901b3bc8d26d1b8b
Chelsea <chelsea_bailey@md-20013613.wgbh.org>
2014-07-10 15:44:02 -0400

    fixed windows phone scroll
    also margins, also cleaned up playVideo

--- js/scrolls.js
+++ js/scrolls.js
@@ -2,19 +2,20 @@ $(document).ready(function() {
 
 	var duration = 1000;
 	var $root = $('html, body');
-	var checkPoint = $('#grid2').offset().top;
+	var $checkPoint = $('#grid2').offset().top;
 
 	$('a[href^="#"]').click(function(event) {
 		event.preventDefault();
-		$root.animate({scrollTop:$( $(this).attr('href')).position().top}, duration);
-		console.log($(this).attr('href'))
+		$root.animate({scrollTop:$($(this).attr('href')).position().top}, duration);
+		//for windows phone additional animation
+		$root.animate({scrollTop:$($(this).attr('href')).position().top}, duration);
 		return false;
 	});
 	
 	$('.topReturn').css({ left: '-1em'});
 
 	$(window).scroll(function() {
-		if ($(this).scrollTop() > checkPoint) {
+		if ($(this).scrollTop() > $checkPoint) {
 			$('.topReturn').fadeIn(duration);
 		} else {
 			$('.topReturn').fadeOut(duration);
